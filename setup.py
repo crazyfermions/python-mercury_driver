823092f813c79346e2d6abdb7a8445b47f50cef5
Sam Schott <ss2151@cam.ac.uk>
2018-09-20 20:34:27 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(name="MercuryiTC",
+      version="0.1.0",
+      description="Full Python driver for the Oxford Mercury iTC cryogenic environment controller.",
+      url="https://github.com/OE-FET/MercuryiTC.git",
+      author="Sam Schott",
+      author_email="ss2151@cam.ac.uk",
+      licence='MIT',
+      long_description=open('README.md').read(),
+      packages=find_packages(),
+      install_requires=[
+          'PyVISA',
+          'pyvisa-py',
+          'visa',
+          'setuptools',
+      ],
+      zip_safe=False,
+)
