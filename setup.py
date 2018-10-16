d775b69739d10041d9c8d1f4342bb8b85712b9a9
Sam Schott <ss2151@cam.ac.uk>
2018-10-16 12:41:51 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name="mercuryitc",
       version="0.1.0",
       description="Full Python driver for the Oxford Mercury iTC cryogenic environment controller.",
-      url="https://github.com/OE-FET/MercuryiTC.git",
+      url="https://github.com/OE-FET/mercuryitc.git",
       author="Sam Schott",
       author_email="ss2151@cam.ac.uk",
       licence='MIT',
