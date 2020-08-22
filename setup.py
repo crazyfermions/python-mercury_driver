a735b441083bacaf4b15c51f27100b90cb6073a9
Sam Schott <ss2151@cam.ac.uk>
2020-08-22 18:58:14 +0200

    use uniform quotes

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ from setuptools import setup, find_packages
 # Florian Forster <florian.forster@rwth-aachen.de>
 # Sam Schott <ss2151@cam.ac.uk>
 
-setup(name="mercuryitc",
+setup(name='mercuryitc',
       version='0.2.4',
-      description="Full Python driver for the Oxford Mercury iTC cryogenic environment controller.",
+      description='Full Python driver for the Oxford Mercury iTC cryogenic environment controller.',
       author='Florian Forster, Sam Schott',
       maintainer='Florian Forster',
       maintainer_email='florian.forster@rwth-aachen.de',
       url='https://github.com/crazyfermions/python-mercury_driver',
       licence='MIT',
       long_description=open('README.md').read(),
-      long_description_content_type="text/markdown",
+      long_description_content_type='text/markdown',
       packages=find_packages(),
       install_requires=[
           'PyVISA',
