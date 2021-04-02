549a1cfa046478dda14e99bbf8672bd6d55cac24
Sam Schott <ss2151@cam.ac.uk>
2021-04-02 12:20:41 +0100

    switch to f-strings

--- mercuryitc/mercury_driver.py
+++ mercuryitc/mercury_driver.py
@@ -89,7 +89,7 @@ class CachedPropertyContainer(object):
         ignored_delimiters (int): Amount of ':' which belongs to the property's
             value. Only makes sense for str values.
         """
-        q = 'READ:%s:%s' % (self.address, name)
+        q = f'READ:{self.address}:{name}'
         resp = self.query(q)
         delimiters = resp.count(':')
         value = resp.split(':', delimiters-ignored_delimiters)[-1]
@@ -98,13 +98,13 @@ class CachedPropertyContainer(object):
 
     def _write_property(self, name, value, convert):
         if convert == float:
-            value_str = '%f' % value
+            value_str = f'{value:f}'
         elif convert == int:
-            value_str = '%i' % value
+            value_str = f'{int(value):d}'
         else:
             value_str = value
 
-        q = 'SET:%s:%s:%s' % (self.address, name, value_str)
+        q = f'SET:{self.address}:{name}:{value_str}'
         resp = self.query(q).split(':')
         if resp[-1] != 'VALID':
             print(resp)
@@ -185,7 +185,7 @@ class MercuryModule(MercuryCommon):
         self._cache = {}
 
     def __repr__(self):
-        return '<%s(%s, %s)>' % (type(self).__name__, self.nick, self.parent)
+        return f'<{type(self).__name__}({self.nick}, {self.parent})>'
 
     def read(self):
         return self.parent.read()
@@ -267,7 +267,7 @@ class MercuryITC_HTR(MercuryModule):
             val = [str(v) for v in val]
             self._write_property('SIG:VOLT', ''.join(val), str)
         else:
-            raise ValueError('Only values from 0 to %s allowed.' % self.vlim)
+            raise ValueError(f'Only values from 0 to {self.vlim} allowed')
 
     @property
     def curr(self):
@@ -293,7 +293,7 @@ class MercuryITC_HTR(MercuryModule):
         if 0 <= val <= self.pmax:
             self._write_property('SIG:PWR', val, float)
         else:
-            raise ValueError('Only values from 0 to %s allowed' % self.pmax)
+            raise ValueError(f'Only values from 0 to {self.pmax} allowed')
 
 
 class MercuryITC_TEMP(MercuryModule):
@@ -313,7 +313,7 @@ class MercuryITC_TEMP(MercuryModule):
         if val in self.TYPES:
             self._write_cached_property('TYPE', val, str)
         else:
-            raise ValueError('Only values from %s allowed' % self.TYPES)
+            raise ValueError(f'Only values from {self.TYPES} allowed')
 
     @type.deleter
     def type(self):
@@ -327,7 +327,7 @@ class MercuryITC_TEMP(MercuryModule):
     @exct_type.setter
     def exct_type(self, val):
         if val not in self.TYPES:
-            raise ValueError('Only values from %s allowed' % self.EXCT_TYPES)
+            raise ValueError(f'Only values from {self.EXCT_TYPES} allowed')
         else:
             self._write_cached_property('EXCT:TYPE', val, str)
 
@@ -379,7 +379,7 @@ class MercuryITC_TEMP(MercuryModule):
         if val in self.CAL_INT:
             self._write_cached_property('CAL:INT', val, str)
         else:
-            raise ValueError('Only values from %s allowed' % self.CAL_INT)
+            raise ValueError(f'Only values from {self.CAL_INT} allowed')
 
     @cal_int.deleter
     def cal_int(self):
@@ -467,7 +467,7 @@ class MercuryITC_TEMP(MercuryModule):
         if val in nick_list:
             self._write_cached_property('LOOP:HTR', val, str)
         else:
-            raise ValueError('Only values in %s allowed.' % nick_list)
+            raise ValueError(f'Only values in {nick_list} allowed')
 
     @loop_htr.deleter
     def loop_htr(self):
@@ -486,7 +486,7 @@ class MercuryITC_TEMP(MercuryModule):
         if val in nick_list:
             self._write_cached_property('LOOP:AUX', val, str)
         else:
-            raise ValueError('Only values in %s allowed.' % nick_list)
+            raise ValueError(f'Only values in {nick_list} allowed')
 
     @loop_aux.deleter
     def loop_aux(self):
@@ -790,7 +790,7 @@ class MercuryITC(MercuryCommon):
         self.connect(**kwargs)
 
     def __repr__(self):
-        return '<%s(%s)>' % (type(self).__name__, self.visa_address)
+        return f'<{type(self).__name__}({self.visa_address})>'
 
     def connect(self, **kwargs):
 
@@ -807,10 +807,10 @@ class MercuryITC(MercuryCommon):
                             'that no other program is connected.')
                 self.connection = None
             except AttributeError:
-                logger.info('Invalid VISA address %s.' % self.visa_address)
+                logger.info(f'Invalid VISA address {self.visa_address}')
                 self.connection = None
             except Exception:
-                logger.info('Could not connect to Mercury at %s.' % self.visa_address)
+                logger.info(f'Could not connect to Mercury at {self.visa_address}')
                 self.connection = None
 
     def disconnect(self):
@@ -856,7 +856,7 @@ class MercuryITC(MercuryCommon):
         with self._lock:
             if not self.connection:
                 raise ConnectionError('Not connected to device.')
-            r = self.connection.query('%s' % q)
+            r = self.connection.query(str(q))
             self.connection.clear()
             return r
 
@@ -894,7 +894,7 @@ class MercuryITC(MercuryCommon):
     @user.setter
     def user(self, val):
         if val not in self.USERS:
-            raise ValueError('Only values from %s allowed' % self.USERS)
+            raise ValueError(f'Only values from {self.USERS} allowed')
         else:
             self._write_cached_property('USER', val, str)
 
@@ -911,7 +911,7 @@ class MercuryITC(MercuryCommon):
     @dima.setter
     def dima(self, val):
         if val not in self.DIMAS:
-            raise ValueError('Only values from %s allowed' % self.DIMAS)
+            raise ValueError(f'Only values from {self.DIMAS} allowed')
         else:
             self._write_cached_property('DISP:DIMA', val, str)
 
@@ -962,15 +962,15 @@ class MercuryITC(MercuryCommon):
         if hh < 0 or hh > 23:
             raise ValueError('Hours must be between 0 and 24')
         else:
-            hh = ('%i' % hh).zfill(2)
+            hh = str(hh).zfill(2)
         if mm < 0 or mm > 59:
             raise ValueError('Minutes must be between 0 and 60')
         else:
-            mm = ('%i' % mm).zfill(2)
+            mm = str(mm).zfill(2)
         if ss < 0 or ss > 59:
             raise ValueError('Seconds must be between 0 and 60')
         else:
-            ss = ('%i' % ss).zfill(2)
+            ss = str(ss).zfill(2)
         val = ':'.join([hh, mm, ss])
         self._write_property('TIME', val, str)
 
@@ -982,15 +982,15 @@ class MercuryITC(MercuryCommon):
     @date.setter
     def date(self, val):
         yyyy, mm, dd = [int(s) for s in val.split(':')]
-        yyyy = ('%i' % yyyy).zfill(4)
+        yyyy = str(yyyy).zfill(4)
         if mm < 1 or mm > 12:
             raise ValueError('Month must be between 1 and 12')
         else:
-            mm = ('%i' % mm).zfill(2)
+            mm = str(mm).zfill(2)
         if dd < 0 or dd > 31:
             raise ValueError('Days must be between 0 and 31')
         else:
-            dd = ('%i' % dd).zfill(2)
+            dd = str(dd).zfill(2)
         val = ':'.join([yyyy, mm, dd])
 
         # Note that this might not validate, since the implementation of the
@@ -1017,10 +1017,10 @@ class MercuryITCFactory(object):
         Create new instance for a new visa_address, otherwise return existing instance.
         """
         if args[0] in cls._instances:
-            logger.debug('Returning existing instance with address %s.' % args[0])
+            logger.debug('Returning existing instance with address %s', args[0])
             return cls._instances[args[0]]
         else:
             instance = MercuryITC(*args, **kwargs)
             cls._instances[args[0]] = instance
-            logger.debug('Creating new instance with address %s.' % args[0])
+            logger.debug('Creating new instance with address %s', args[0])
             return instance
